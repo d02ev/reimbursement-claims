0e75a7d02c8e005a54f8e90b0d053ffafb7d75d2
d02ev <vikramadityapratapsingh98@gmail.com>
2023-02-25 18:47:38 +0530

    add auth middleware

---
+++ server/middlewares/auth/auth.middleware.js
@@ -0,0 +1,18 @@
+const JWT = require('jsonwebtoken');
+const HttpError = require('http-errors');
+
+const verifyAuthToken = (req, res, next) => {
+    try {
+        let authToken = req.headers.authorization;
+        if (!authToken) return next(HttpError.Forbidden('No Auth Token Found!'));
+
+        authToken = authToken.slice(7);
+        const decodedToken = JWT.verify(authToken, process.env.SECRET_KEY);
+        req.user = decodedToken;
+    }
+    catch (err) {
+        return next(HttpError.Unauthorized('You Are Not Authorized For The Action!'));
+    }
+
+    return next();
+};
