cd17ad5a3b79f420c0a38b234488549cd836cfed
d02ev <vikramadityapratapsingh98@gmail.com>
2023-02-27 12:23:46 +0530

    add error services

---
+++ client/src/app/services/error.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorService {
+  // private attribute
+  private _errorMessage: string = '';
+
+  constructor() { }
+
+  // getters and setters
+  set errorMessage(message: string) {
+    this._errorMessage = message;
+  }
+  get errorMessage(): string {
+    return this._errorMessage;
+  }
+}
