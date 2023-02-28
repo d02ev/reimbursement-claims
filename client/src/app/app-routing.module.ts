a451f82d3ed1a335054dc6ae081066c95833bf13
d02ev <vikramadityapratapsingh98@gmail.com>
2023-02-28 11:41:41 +0530

    add user-home route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DefaultHomeComponent } from './default-home/default-home.component';
+import { UserHomeComponent } from './user-home/user-home.component';
 
 const routes: Routes = [
   { path: '', component: DefaultHomeComponent },
   { path: 'auth/login', component: LoginComponent },
-  { path: 'auth/register', component: RegisterComponent }
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'user/dashboard', component: UserHomeComponent }
 ];
 
 @NgModule({
