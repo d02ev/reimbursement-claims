05c144bb67c21a294df01e88fc3283ccdbd2423c
d02ev <vikramadityapratapsingh98@gmail.com>
2023-03-01 23:23:28 +0530

    update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,17 +3,35 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DefaultHomeComponent } from './default-home/default-home.component';
+import { AuthGuard } from './guards/auth.guard';
+import { UserGuard } from './guards/user.guard';
+import { CreateReimbursementComponent } from './reimbursements/create-reimbursement/create-reimbursement.component';
+import { EditReimbursementComponent } from './reimbursements/edit-reimbursement/edit-reimbursement.component';
 import { UserHomeComponent } from './user-home/user-home.component';
 
 const routes: Routes = [
   { path: '', component: DefaultHomeComponent },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
-  { path: 'user/dashboard', component: UserHomeComponent }
+  {
+    path: 'user/dashboard',
+    component: UserHomeComponent,
+    canActivate: [AuthGuard, UserGuard],
+  },
+  {
+    path: 'user/create',
+    component: CreateReimbursementComponent,
+    canActivate: [AuthGuard, UserGuard],
+  },
+  {
+    path: 'user/edit/:claimId',
+    component: EditReimbursementComponent,
+    canActivate: [AuthGuard, UserGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
